1a05dfb3201ad486aff15f9f8a45bff904e756c8
unknown <shahariar.anu@northsouth.edu>
2021-12-22 23:26:41 +0600

    136A Presents

---
+++ A-Set/136A_Presents.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    for(int j=1; j<=n; j++)
+    {
+        for(int i=0; i<n; i++)
+            if(arr[i]==j)
+                cout<<i+1<<" ";
+    }
+    return 0;
+}
