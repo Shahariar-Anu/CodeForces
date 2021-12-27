19693524f3d52a3cb5edcbb8e1ef5771af2275b9
unknown <shahariar.anu@northsouth.edu>
2021-12-27 11:25:21 +0600

    1371A Magical Sticks

---
+++ A-Set/1371A_Magical_Sticks.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int count=n/2;
+        if(n%2==1)
+            count++;
+        cout<<count<<"\n";
+    }
+    return 0;
+}
