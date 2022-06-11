5190be4295e5cad5a1d81679a457112eb01527b6
unknown <shahariar.anu@northsouth.edu>
2022-06-11 21:33:22 +0600

    1285A Mezo playing zoma

---
+++ A-Set/1285A_Mezo_Playing_Zoma.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    cout<<n+1;
+    return 0;
+}
