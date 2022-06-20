8bf0aa660dccac560214d16db3ba1b0cea5c8492
unknown <shahariar.anu@northsouth.edu>
2022-06-20 23:21:12 +0600

    1186A Vus the Cossack and a Contest

---
+++ A-Set/1186A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(n>m || n>k)
+        cout<<"No";
+    else
+        cout<<"Yes";
+    return 0;
+}
