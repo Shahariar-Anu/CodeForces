10e01b20797e59e4a63afbc856d80b1a31729161
unknown <shahariar.anu@northsouth.edu>
2022-07-01 23:12:37 +0600

    1372A Omkar and Completion codeforces

---
+++ A-Set/1372A_Omkar_and_Completion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0; i<n; i++)
+        {
+            cout<<"2 ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
