5a2349730c5a13a8baca93c0c1f18faaf7f7b824
unknown <shahariar.anu@northsouth.edu>
2023-10-18 20:04:38 +0600

    711A Bus to Udayland

---
+++ A-Set/711A_Bus_to_Udayland.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n,check=0;
+    string s[1000];
+    cin>>n;
+    for(int i=0; i<n; i++)
+        cin>>s[i];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<s[i].length()-1; j++)
+        {
+            if(s[i][j]=='O' && s[i][j+1]=='O')
+            {
+                s[i][j]='+';
+                s[i][j+1]='+';
+                check=1;
+                break;
+            }
+        }
+        if(check)
+            break;
+    }
+    if(check)
+    {
+        cout<<"YES\n";
+        for(int i=0; i<n; i++)
+            cout<<s[i]<<"\n";
+    }
+    else
+        cout<<"NO\n";
+    return 0;
+}
+
+
