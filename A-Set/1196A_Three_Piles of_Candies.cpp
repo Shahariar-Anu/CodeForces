3e06cc41146481ea358ead411fc98e647e571817
unknown <shahariar.anu@northsouth.edu>
2023-11-11 21:45:33 +0600

    1196A Three Piles of Candies

---
+++ A-Set/1196A_Three_Piles of_Candies.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a,b,c;
+        cin>>a>>b>>c;
+        cout<<(a+b+c)/2<<"\n";
+    }
+    return 0;
+}
+
