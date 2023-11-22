549fd2746dca30e378f2d931fa92526a73a92d50
unknown <shahariar.anu@northsouth.edu>
2023-11-22 21:33:22 +0600

    1594A Consecutive Sum Riddle

---
+++ A-Set/1594A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n,l;
+        cin>>n;
+        l=-(n-1);
+        cout<<l<<" "<<n<<"\n";
+    }
+    return 0;
+}
+
