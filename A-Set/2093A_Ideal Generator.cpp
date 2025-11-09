adb27d5fc63dd0726e2e261d5227eabbbe6324ec
unknown <shahariar.anu@northsouth.edu>
2025-11-10 03:08:41 +0600

    2093A Ideal Generator

---
+++ A-Set/2093A_Ideal Generator.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t,k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k;
+        if(k%2!=0)
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
+
+}
