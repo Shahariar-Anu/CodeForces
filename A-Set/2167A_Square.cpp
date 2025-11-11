49100256bf0267101e1512391092c27c09b30a29
unknown <shahariar.anu@northsouth.edu>
2025-11-11 19:12:16 +0600

    2167A Square

---
+++ A-Set/2167A_Square.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);  cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a==b && b==c && c==d)
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
+}
