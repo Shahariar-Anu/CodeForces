f13c8e6cccb1f9cb9727f6725a91bfc470698602
unknown <shahariar.anu@northsouth.edu>
2025-11-14 06:24:12 +0600

    1968A Maximize

---
+++ A-Set/1968A_Maximize.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x;
+        cout<<x-1<<"\n";
+    }
+}
