e503d5cabdf5dbabd48d1a21ea92b0f8f768af8f
Shahariar Anu <73732861+Shahariar-Anu@users.noreply.github.com>
2026-05-02 23:47:41 +0600

    updated code of 1997A_Strong Password

--- A-Set/1997A_Strong Password.cpp
+++ A-Set/1997A_Strong Password.cpp
@@ -4,32 +4,46 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
-    cin>>t;
-    string s;
-    cin>>s;
-    bool f = true;
-    int n = s.size();
-    for(int i=0; i<n-1; i++)
+    cin >> t;
+
+    while(t--)
     {
-        cout<<s[i];
-        if(s[i+1] == s[i] && f)
+        string s;
+        cin >> s;
+
+        bool f = true;
+        int n = s.size();
+
+        for(int i = 0; i < n - 1; i++)
         {
-            f = false;
-            if(s[i] == 'a')
-                cout<<"b";
-            else
-                cout<<"a";
+            cout << s[i];
+
+            if(s[i + 1] == s[i] && f)
+            {
+                f = false;
+
+                if(s[i] == 'a')
+                    cout << "b";
+                else
+                    cout << "a";
+            }
         }
+
+        cout << s[n - 1];
+
+        if(f)
+        {
+            if(s[n - 1] == 'a')
+                cout << "b";
+            else
+                cout << "a";
+        }
+
+        cout << "\n";
     }
-    cout<<s[n-1];
-    if(f)
-    {
-        if(s[n-1] == 'a')
-            cout<<"b";
-        else
-            cout<<"a";
-    }
-    cout<<"\n";
+
     return 0;
 }
